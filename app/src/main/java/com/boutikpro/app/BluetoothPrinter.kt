55ae0aef5f54c1ed9892a751093fef6c89dcff3c
arsene0010 <smartprocesslabsolution@gmail.com>
2026-07-24 13:58:08 +0100

    Create BluetoothPrinter.kt
    RAS

---
+++ app/src/main/java/com/boutikpro/app/BluetoothPrinter.kt
@@ -0,0 +1,7 @@
+package com.boutikpro.app.utils
+
+class BluetoothPrinter {
+    fun imprimerTicket(produits: List<String>, total: Double) {
+        // Code pour imprimante thermique a ajouter apres
+    }
+}
